6e7e53300e1f6a9c00d81d1cd9e4cbef716d2c32
GtDavide01 <davidefiorini01@gmail.com>
2022-11-24 14:39:59 +0100

    esercizio concluso

--- paragrafo.php
+++ paragrafo.php
@@ -1,4 +1,7 @@
-<?php ?>
+<?php
+$userPar = $_GET["paragrafo"];
+$userWordCen = $_GET["word"];
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +16,35 @@
 </head>
 
 <body>
+    <div class="container">
+        <div class="paragrafonormale">
+            <h2>Paragrafo</h2>
+            <br>
+            <p>
+                <?php echo $userPar; ?>
+            </p>
+            <h3>Lunghezza :
+                <?php
+                echo strlen($userPar);
+                ?>
+            </h3>
+        </div>
+        <div class="paragrafocensurato">
+            <h2>Paragrafo Censurato </h2>
+            <p>
+                <?php
+                $paragrafoSostituito = str_replace("$userWordCen", "***", "$userPar");
+                echo $paragrafoSostituito;
+                ?>
+            </p>
+            <h3>Lunghezza :
+                <?php
+                echo strlen($paragrafoSostituito);
+                ?>
+            </h3>
+        </div>
+
+    </div>
 
 </body>
 
